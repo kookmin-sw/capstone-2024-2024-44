57f9c3ff114ed2ef7a3140dfa4844260abbf138a
sjykmucs <star01070@kookmin.ac.kr>
2024-05-17 12:38:59 +0900

    add firebase settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,7 @@
 plugins {
+    // id("com.android.application")
+    // Add the Google services Gradle plugin
+    id("com.google.gms.google-services")
     alias(libs.plugins.android.application)
 }
 
@@ -37,4 +40,14 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
+
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-analytics")
+
+    // Add the dependencies for any other desired Firebase products
+    // https://firebase.google.com/docs/android/setup#available-libraries
 }
